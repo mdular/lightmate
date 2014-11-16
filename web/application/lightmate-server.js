90266f0e1639fa1aeeaaa5ec486162fd79da308a
mdular <git@the-enemy.org>
2014-11-16 20:31:33 +0100

    add validation, cleanup

--- web/application/lightmate-server.js
+++ web/application/lightmate-server.js
@@ -1,14 +1,16 @@
-var mongoURI = 'mongodb://127.0.0.1:27017',
-    MongoClient = require('mongodb').MongoClient,
+var MongoClient = require('mongodb').MongoClient,
     db,
     http = require('http'),
     server;
 
 var config = {
+  mongoURI: 'mongodb://localhost:27017/lightmate',
   maxPostLength: 1e6, // 1.000.000 bytes
+  pixelAmount: 64,
+  historyLength: 10,
   defaultHeader: {
-    // TODO: set proper origins when deploying this..
-    "Access-Control-Allow-Origin" : "*" 
+    // TODO: set proper origins when deploying this!
+    "Access-Control-Allow-Origin" : "*"
   }
 }
 
@@ -16,40 +18,38 @@ function requestHandler(req, response) {
   var params = req.url.split('/');
   params.shift();
 
-  console.dir(req.method);
-
   if (req.method === 'GET') {
-    try {
-      routeRequest(params, response, null);
-    } catch (err) {
-      sendError(response, 400); // Bad Request
-      //response.end();
-    }
+    routeRequest(params, response, null);
   } else if (req.method === 'POST') {
+    // TODO: csrf token validation
+    //       (needs to be persisted and rendered in template too...)
+    // currently this is an unlimited open write!
+
     var body = '';
     req.on('data', function (data) {
       body += data;
 
       if (body.length > config.maxPostLength) {
         sendError(response, 413); // Request Entity Too Large
-        //response.end();
         request.connection.destroy();
       }
     });
     req.on('end', function () {
-      try {
-        var data = JSON.parse(body);
-        // TODO: sanitize
-        routeRequest(params, response, data);
-      } catch (err) {
-        sendError(response, 400); // Bad Request
-        //response.end();
-      }
+      var data = JSON.parse(body);
+
+      routeRequest(params, response, data);
     });
   }
 }
 
 function routeRequest(params, response, postData) {
+  // TODO: serve static files
+  // TODO: default route
+  // TODO: route actions
+  // TODO: simple render
+
+  console.log("request", params);
+
   if (typeof params[1] === 'undefined') {
     if (params[0] === 'favicon.ico') {
       return;
@@ -59,30 +59,72 @@ function routeRequest(params, response, postData) {
 
     actions.save(params[0], postData, response);
   } else {
-    throw new Error();
+    sendError(response, 404, 'Not found');
   }
+
+  // TODO: test if action exists
+  // TODO: call actions with try catch, log error
 }
 
 var actions = {
-
   save  : function (id, data, response) {
-    // prepare data
-    var update = {};
-    if (typeof data.pixels !== 'undefined' && data.pixels.length > 0) {
-      update['$set'] = { pixels : data.pixels };
+    var invalid = false,
+        update = {};
+
+    // TODO: store IPs / User with history
+
+    // prepare update data
+    // TODO: strict validation for expected values!
+    // validate pixels
+    if (typeof data.pixels !== 'undefined' && data.pixels.length == config.pixelAmount) {
+      update.pixels = data.pixels;
+    } else {
+      invalid = true;
     }
-    if (typeof data.history !== 'undefined' && data.history.length > 0) {
-      update['$set'] = { history : data.history };
+
+    // validate history
+    if (!invalid && typeof data.history !== 'undefined') {
+      if (data.history.length > config.historyLength) {
+        invalid = true;
+      }
+
+      if (data.history.length > 0) {
+        for (var i = 0, len = data.history.length; i < len; i++) {
+          var frame = data.history[i].frame;
+
+          if (typeof frame === 'undefined' || frame.length !== config.pixelAmount) {
+            invalid = true;
+          }
+        }
+      }
+
+      if (!invalid) {
+        update.history = data.history ;
+      }
+    }
+
+    if (invalid) {
+      console.dir('invalid save request', data);
+      sendError(response, 400); // Bad request
+      return;
     }
 
     db.collection('frames').findAndModify(
-      { reference : id }, // query
-      { reference : 1}, // sort
-      update, // update doc
-      { // options
+      // query
+      { reference : id },
+
+      // sort
+      { reference : 1},
+
+      // update
+      { '$set' : update},
+
+      // options
+      {
         new   : true,
         upsert: true
-      }, 
+      },
+
       function (err, doc) {
         if (err) {
           console.dir(err);
@@ -90,9 +132,8 @@ var actions = {
           return;
         }
 
-        sendHeader(response, 200, 'application/x-javascript');
-        response.write(JSON.stringify(doc));
-        response.end();
+        delete doc._id;
+        sendJSONResponse(response, doc);
     });
   },
 
@@ -100,7 +141,7 @@ var actions = {
     db.collection('frames').findOne(
     {
       reference : id
-    }, 
+    },
     {
       _id     : 0,
       pixels  : 1,
@@ -109,15 +150,14 @@ var actions = {
     function (err, doc) {
       if (err) {
         console.dir(err);
+        sendError(response, 500); // Internal server error
         return;
       }
 
       if (!doc) {
-        sendError(response, 404, 'not found'); // Not found
+        sendError(response, 404, 'Not Found'); // Not found
       } else {
-        sendHeader(response, 200, 'application/x-javascript');
-        response.write(JSON.stringify(doc));
-        response.end();
+        sendJSONResponse(response, doc);
       }
     });
   }
@@ -138,12 +178,22 @@ function sendHeader(response, statusCode, contentType) {
   response.writeHead(statusCode, header);
 }
 
-MongoClient.connect('mongodb://localhost:27017/lightmate', function (err, dbConnection) {
+function sendJSONResponse(response, data) {
+  sendHeader(response, 200, 'application/x-javascript');
+  response.write(JSON.stringify(data));
+  response.end();
+}
+
+MongoClient.connect(config.mongoURI, function (err, dbConnection) {
   if (err) throw err;
+
+  // TODO: handle mongo connection loss?
 
   db = dbConnection;
   console.dir('mongodb connection established');
 
+  // TODO: handle server independently of app / db connection
+  //       > just route / static files
   server = http.createServer(requestHandler);
   server.listen(8888);
 
