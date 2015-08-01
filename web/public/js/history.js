e9dcbd75b3982c3208b18ab64c04c5b9db33b977
Markus J Doetsch <git@the-enemy.org>
2015-08-01 20:04:47 +0200

    indentation

--- web/public/js/history.js
+++ web/public/js/history.js
@@ -8,7 +8,7 @@ app.registerModule('history', function () {
     "use strict";
 
     var config = {
-      items : 10 // how much history to keep
+        items : 10 // how much history to keep
     };
 
     var history,
@@ -16,99 +16,99 @@ app.registerModule('history', function () {
         ui;
 
     var init = function () {
-      ui = app.getModule('ui').module;
-      history = document.querySelector('#histories');
+        ui = app.getModule('ui').module;
+        history = document.querySelector('#histories');
 
-      // listen for draw events
-      pixels.addEventListener('draw', function (event) {
-        var historyState = {
-          mode  : event.detail.mode,
-          frame : ui.getFrame()
-        }
+        // listen for draw events
+        pixels.addEventListener('draw', function (event) {
+            var historyState = {
+                mode  : event.detail.mode,
+                frame : ui.getFrame()
+            }
 
-        addItem(historyState);
-      });
+            addItem(historyState);
+        });
 
-      // listen for clicks on history states
-      history.addEventListener('click', function (event) {
-        var re = /history/g;
-        if ( !re.test(event.target.getAttribute('class')) ) {
-          return;
-        }
+        // listen for clicks on history states
+        history.addEventListener('click', function (event) {
+            var re = /history/g;
+            if ( !re.test(event.target.getAttribute('class')) ) {
+                return;
+            }
 
-        var index = getHistoryIndex(event.target);
-        ui.setFrame(historyData[index].frame);
-      });
+            var index = getHistoryIndex(event.target);
+                ui.setFrame(historyData[index].frame);
+        });
     };
 
     var addItem = function (historyState) {
-      // add item to front of array
-      historyData.unshift(historyState);
 
-      // cap states array
-      if (historyData.length > config.items) {
-        historyData.pop();
-      }
+        // add item to front of array
+        historyData.unshift(historyState);
 
-      // create from template
-      var item = ui.getTemplate('history');
+        // cap states array
+        if (historyData.length > config.items) {
+            historyData.pop();
+        }
 
-      item.innerHTML = renderHistory(historyState);
+        // create from template
+        var item = ui.getTemplate('history');
 
-      history.insertBefore(item, history.childNodes[0]);
+        item.innerHTML = renderHistory(historyState);
 
-      if (history.childNodes.length > config.items) {
-        removeItem(history.childNodes.length -1);
-      }
+        history.insertBefore(item, history.childNodes[0]);
+
+        if (history.childNodes.length > config.items) {
+            removeItem(history.childNodes.length -1);
+        }
     };
 
     var renderHistory = function (historyState) {
-      var html = '',
+        var html = '',
           thumb = '<div class="thumb">';
 
-      for (var i = 0; i < historyState.frame.length; i++) {
-        thumb += '<span style="background: '+ historyState.frame[i] +';"></span>';
-      }
-      thumb += '</div>';
+        for (var i = 0; i < historyState.frame.length; i++) {
+            thumb += '<span style="background: '+ historyState.frame[i] +';"></span>';
+        }
+        thumb += '</div>';
 
-      // TODO: add change event msg of action
+        // TODO: add change event msg of action
 
-      html = thumb;
-      html += historyState.mode;
+        html = thumb;
+        html += historyState.mode;
 
-      return html;
+        return html;
     };
 
     var getHistoryIndex = function (target) {
-      var i = 0;
-      while ((target = target.previousElementSibling) !== null) {
-        i++;
-      }
+        var i = 0;
+        while ((target = target.previousElementSibling) !== null) {
+            i++;
+        }
 
-      return i;
+        return i;
     };
 
     var removeItem = function (index) {
-      historyData.splice(index, 1);
+        historyData.splice(index, 1);
 
-      history.removeChild(history.childNodes[index]);
+        history.removeChild(history.childNodes[index]);
     };
 
     var getHistory = function () {
-      return historyData;
+        return historyData;
     };
 
     var setHistory = function (data) {
-
-      // clear history
-      for (var i = 0, len = historyData.length; i < len; i++) {
+        // clear history
+        for (var i = 0, len = historyData.length; i < len; i++) {
         removeItem(0);
-      }
+        }
 
-      // add new items
-      for (var i = data.length - 1; i >= 0; i--) {
+        // add new items
+        for (var i = data.length - 1; i >= 0; i--) {
         addItem(data[i]);
-      }
+        }
     };
 
     return {
