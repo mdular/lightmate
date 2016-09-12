1f7d80d625eef245a505a35bd226e2d0544d48ab
Markus J Doetsch <git@the-enemy.org>
2016-09-12 09:19:59 +0200

    client process: connect to socket server, listen for draw command, write data to serial port

--- client/client.js
+++ client/client.js
@@ -1,49 +1,83 @@
-// run
+var net = require ("net");
+var SerialPort = require('./SerialPort');
+
+// TODO: receive a token first from http server (to link with account)
+// TODO: watch connections (socket, serial)
+// TODO: attempt reconnects (socket, serial)
+// TODO: report status (socket, serial)
+
+var connected = false;
 
 // connect to server
+var connection = net.createConnection({
+    port: 8124,
+    host: 'localhost'
+}, () => {
+    console.log('connected to server');
+
+    // TODO: handle connection failures, retry
+
+    // TODO: send connection package
+    connection.write(JSON.stringify({
+        action: 'connect',
+        payload: {
+            ref: 'A',
+            token: 123
+        }
+    }));
+});
+
+connection.on('data', (data) => {
+    try {
+        data = JSON.parse(data);
+    } catch (err) {
+        console.log("Error: json parsing failed", err);
+        return;
+    }
+
+    // handle data.action
+    switch (data.action) {
+        case 'connect':
+            if (data.result !== 'OK') {
+                // TODO: retry?
+                console.log('Socket Error: connection attempt failed', data.result);
+                break;
+            }
+
+            connected = true;
+            break;
+        case 'draw':
+            console.log('draw command pushed');
+
+            // TODO: validate payload
+
+            for (let i in data.payload) {
+                let val = data.payload[i];
+
+                if (val !== 0) {
+                    val = val.replace(/#/, '');
+                  //   val = parseInt(val, 16);
+                  SerialPort.queue(val);
+                } else {
+                  SerialPort.queue('000000');
+                }
+            }
+
+            break;
+        case 'error':
+            console.log('Socket Error: received error message', data.result)
+            break;
+        default:
+            console.log('Socket Error: action could not be handled', data.action);
+            break;
+    }
+});
+
 // connect to serialport
 var SerialPort = require('./SerialPort');
 SerialPort.start(process.env.PORT);
 SerialPort.listen(function (message) {
+    // TODO: process response from arduino: "frame complete"
+
     console.log('recieved serial data:', message);
 })
-
-// watch connections
-// attempt reconnects
-// report status
-
-
-
-// receive frames
-
-
-// write via serial queue
-// write to serialport
-for (let i in data) {
-    let val = data[i];
-
-    // simulate 4 pixel (24 byte) packet
-  //   if (i > 15) {
-  //       break;
-  //   }
-
-    if (val !== 0) {
-        val = val.replace(/#/, '');
-      //   val = parseInt(val, 16);
-
-      //   var r = (val >> 16) & 255;
-      //   var g = (val >> 8) & 255;
-      //   var b = val & 255;
-
-      //   console.log( r + "," + g + "," + b);
-      //   serial.queue(r);
-      //   serial.queue(g);
-      //   serial.queue(b);
-      SerialPort.queue(val);
-    } else {
-      //   serial.queue(0);
-      //   serial.queue(0);
-      //   serial.queue(0);
-      SerialPort.queue('000000');
-    }
-}
